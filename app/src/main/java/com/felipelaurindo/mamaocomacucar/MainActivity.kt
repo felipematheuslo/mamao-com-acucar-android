24628be42610d3cc4f9936c31a1df00cd3cd868d
Felipe Laurindo <felipe_nu@hotmail.com>
2026-08-16 23:58:34 -0300

    Initial commit

---
+++ app/src/main/java/com/felipelaurindo/mamaocomacucar/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.felipelaurindo.mamaocomacucar
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.felipelaurindo.mamaocomacucar.ui.theme.MamãoComAçúcarTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MamãoComAçúcarTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MamãoComAçúcarTheme {
+        Greeting("Android")
+    }
+}
